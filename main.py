7900b05e89900be9abb728207af9843c8a5fc4a7
devdezzies <boltholt17@gmail.com>
2025-01-24 11:05:22 +0700

    feat: add fizzBuzz functionality on main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import sys 
 
+def fizzBuzz(int: to):
+    for i in range(to):
+        if (i%3==0 && i%5==0):
+            print("FizzBuzz")
+        elif (i%3==0):
+            print("Fizz")
+        elif (i%5==0):
+            print("Buzz")
+
 def main():
     print("Hello world")
 
